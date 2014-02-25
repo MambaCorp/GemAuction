d9cf984eb81269fb1bcfd85f9359801dfa84410a
jbirak <jesjit.birak@careinnovations.com>
2014-02-24 17:51:51 -0800

    Removed unused function in user controller

--- controllers/user/routes.js
+++ controllers/user/routes.js
@@ -36,27 +36,6 @@ routes = function(app){
 	app.get('/register', function(req, res){
 		res.render(path.join(__dirname, '/views/register.jade'), { user: new User() });
 	});
-
-	app.post('/user/add', function(req, res){
-		var user;
-		console.log("POST: ");
-		console.log(req.body);
-		user = new UserModel({
-			email: req.body.email,
-			firstName: req.body.firstName,
-			lastName: req.body.lastName
-		});
-
-		user.save(function (err){
-			if(!err){
-				return console.log("user created: " + user.email);
-			}else{
-				return console.log(err);
-			}
-		});
-
-		return res.redirect("/");
-	});
 };
 
 module.exports = routes;
